966ced73ef88252728cfc5b1a7bca3a350b7cc27
faxcon08 <faxcon-08@mail.ru>
2022-11-16 15:18:31 +0300

    First Task 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        public static final String ANSI_RESET = "\u001B[0m";
-        public static final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_PURPLE = "\u001B[35m";
 
+        // First Task 1.1
+        System.out.println(ANSI_PURPLE+"First Task 1.1"+ANSI_RESET);
+        int clientOS = 0;
+        final int iOs = 0;
+        final int android = 1;
 
+        if(clientOS==iOs)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        else
+            System.out.println("Установите версию прилоежния для Android по ссылке");
+
+        // Second Task 1.2
+        System.out.println(ANSI_PURPLE+"Second Task 1.2"+ANSI_RESET);
     }
 }
