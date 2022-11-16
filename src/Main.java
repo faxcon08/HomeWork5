1986a215fffdfd2ff0c2b25955c42d6b7c7886f4
faxcon08 <faxcon-08@mail.ru>
2022-11-16 14:57:01 +0300

    Add New Branch hw5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        public static final String ANSI_RESET = "\u001B[0m";
+        public static final String ANSI_PURPLE = "\u001B[35m";
 
-        System.out.println("Hello world!");
+
     }
 }
